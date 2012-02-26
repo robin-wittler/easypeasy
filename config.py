8042b014ee2d2dd4077ccc4344914885bd5c1aa8
Robin Wittler <real@the-real.org>
2012-02-26 13:46:56 +0100

    add working config.py

--- config.py
+++ config.py
@@ -2,19 +2,19 @@
 # -*- coding: utf-8 -*-
 
 # in case of sqlite
-#db_connect_string = 'sqlite:///<absolut_path_to_db_file>'
+db_connect_string = 'sqlite:////tmp/easypeasy.db'
 
 # in case of postgres
 #db_connect_string = 'postgresql://<username>:<password>@<host>:<port>/<database>'
 
 # the secrect key to encrypt cookies
-#secret_key = 'Please change me'
+secret_key = 'Please change me'
 
 # to enable debug (use this only for non-production enviroment)
-#debug = False
+debug = True
 
 # the name of the blog
-#blog_name = 'Please change me'
+blog_name = 'Alsobs.de'
 
 # the blog subtitle
-#blog_subtitle = 'Please change me'
+blog_subtitle = 'Die tun nur so als ob!'
