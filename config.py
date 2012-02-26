cd833ff9e7046b144a9344aab8f6de4648547d69
Robin Wittler <real@the-real.org>
2012-02-26 13:42:11 +0100

    disable all config entries, remove all css

--- config.py
+++ config.py
@@ -2,19 +2,19 @@
 # -*- coding: utf-8 -*-
 
 # in case of sqlite
-db_connect_string = 'sqlite:////tmp/easypeasy.db'
+#db_connect_string = 'sqlite:///<absolut_path_to_db_file>'
 
 # in case of postgres
-# db_connect_string = 'postgresql://<username>:<password>@<host>:<port>/<database>'
-
+#db_connect_string = 'postgresql://<username>:<password>@<host>:<port>/<database>'
 
 # the secrect key to encrypt cookies
-secret_key = 'testertest'
+#secret_key = 'Please change me'
 
 # to enable debug (use this only for non-production enviroment)
-debug = True
+#debug = False
 
+# the name of the blog
+#blog_name = 'Please change me'
 
-# the name of the block
-blog_name = 'Alsobs.de'
-blog_subtitle = 'Die tun nur so als ob!'
+# the blog subtitle
+#blog_subtitle = 'Please change me'
